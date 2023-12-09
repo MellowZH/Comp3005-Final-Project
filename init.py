73e170ef44a4b3e19454d2ae378b20601f56d1d4
MellowZH <zachary.hollmann@gmail.com>
2023-12-09 10:25:21 -0500

    Update init.py

--- init.py
+++ init.py
@@ -12,16 +12,55 @@ conn = psycopg2.connect(database = DATABASE_NAME,
 cur = conn.cursor()
 
 try:
-    # Execute a command: create students table
+    # Execute a command: create roles table
+    cur.execute("""CREATE TABLE roles(
+                role_id SERIAL PRIMARY KEY,
+                role_name TEXT NOT NULL)""")
+                
+    # Execute a command: create users table
     cur.execute("""CREATE TABLE users(
                 user_id SERIAL PRIMARY KEY,
+                password TEXT NOT NULL,
                 first_name TEXT NOT NULL,
                 last_name TEXT NOT NULL,
                 email TEXT UNIQUE NOT NULL,
-                password TEXT NOT NULL,
+                phone TEXT UNIQUE NOT NULL,
                 DOB DATE,
-                role_id INT NOT NULL,
+                role INT references roles(role_id))""")
+                
+    # Execute a command: create members table
+    cur.execute("""CREATE TABLE members(
+                user_id PRIMARY KEY references users(user_id),
+                loyalty_points INT,
                 join_date DATE)""")
+    
+    # Execute a command: create trainers table
+    cur.execute("""CREATE TABLE trainers(
+                user_id PRIMARY KEY references users(user_id),
+                start_date DATE)""")
+                
+    # Execute a command: create profiles table
+    cur.execute("""CREATE TABLE profiles(
+                profile_id SERIAL PRIMARY KEY,
+                HRV TEXT NOT NULL,
+                SPO2 TEXT NOT NULL,
+                RHR TEXT NOT NULL,
+                5k_goal TEXT NOT NULL,
+                pushup_goal TEXT NOT NULL,
+                5k_best TEXT NOT NULL,
+                pushup_best TEXT NOT NULL,
+                member INT references members(user_id))""")
+                
+    # Execute a command: create personal sessions table
+    cur.execute("""CREATE TABLE personal_session(
+                ps_id SERIAL PRIMARY KEY,
+                trainer INT references trainers(user_id),
+                member INT references members(user_id))""")
+                
+    # Execute a command: create group sessions table
+    cur.execute("""CREATE TABLE personal_session(
+                gs_id SERIAL PRIMARY KEY,
+                trainer INT references trainers(user_id))""")
     
     # Make the changes to the database persistent
     conn.commit()
