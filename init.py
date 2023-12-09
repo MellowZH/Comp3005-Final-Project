92a6b07a962cb4b49cdf666ecd86922c033bb1e5
MellowZH <zachary.hollmann@gmail.com>
2023-12-09 10:02:17 -0500

    Update init.py

--- init.py
+++ init.py
@@ -13,16 +13,19 @@ cur = conn.cursor()
 
 try:
     # Execute a command: create students table
-    cur.execute("""CREATE TABLE students(
-                student_id SERIAL PRIMARY KEY,
+    cur.execute("""CREATE TABLE users(
+                user_id SERIAL PRIMARY KEY,
                 first_name TEXT NOT NULL,
                 last_name TEXT NOT NULL,
                 email TEXT UNIQUE NOT NULL,
-                enrollment_date DATE)""")
+                password TEXT NOT NULL,
+                DOB DATE,
+                role_id INT NOT NULL,
+                join_date DATE)""")
     
     # Make the changes to the database persistent
     conn.commit()
-    print('Student table created successfully!')
+    print('Users table created successfully!')
 
     # Execute a command: insert initial data into students table
     cur.execute("""INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
